73ed625f707d9f5fbf91e74e4f6707eb159f3cdd
Adam Tovatt <sporedenstore@hotmail.com>
2023-08-19 14:51:23 +0200

    fix cors#

--- RetendoVoice/Program.cs
+++ RetendoVoice/Program.cs
@@ -4,28 +4,31 @@ namespace RetendoVoice
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.AddCors();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+            WebApplication app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+            app.UseCors(options =>
+            {
+                options.AllowAnyHeader()
+                       .AllowAnyMethod()
+                       .SetIsOriginAllowed(origin => true) // Allow any origin
+                       .AllowCredentials(); // Allow credentials (e.g., cookies)
+            });
 
+            app.UseAuthorization();
 
             app.MapControllers();
 
